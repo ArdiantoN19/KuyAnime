214f164c7b1e4623fc5da207d92a8ad38e8335a3
Ardianto Nugroho <ardi19nugroho@gmail.com>
2024-02-03 17:09:34 +0700

    update minor export authoptions int route  [...nextauth]

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,5 @@
-import { NextAuthOptions } from "next-auth";
+import { authOptions } from "@/lib/api/auth";
 import NextAuth from "next-auth/next";
-import GithubProvider from "next-auth/providers/github";
-
-export const authOptions: NextAuthOptions = {
-  secret: process.env.NEXT_AUTH_SECRET,
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token, user }) {
-      session.accessToken = token.accessToken as string;
-      return session;
-    },
-  },
-};
 
 const handler = NextAuth(authOptions);
 
--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,5 +1,28 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 import { getServerSession } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXT_AUTH_SECRET,
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken as string;
+      return session;
+    },
+  },
+};
 
 export const authServerSession = async () => {
   const session = await getServerSession(authOptions);
